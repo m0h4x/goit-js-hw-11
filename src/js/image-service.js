391c727a2ed5ee0423edede2097a6a1a4e183193
m0h4x <86525723+m0h4x@users.noreply.github.com>
2022-01-01 20:14:19 +0100

    image service
    image service js

---
+++ src/js/image-service.js
@@ -0,0 +1,40 @@
+const API_KEY = '24385754-e04213b2da07f3f18967e54df';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const axios = require('axios');
+
+export default class ImagesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+ 
+  }
+
+
+  async fetchImages() {
+   const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}
+   &image_type=photo&orientation=horizontal&safesearch=true
+   &page=${this.page}&per_page=40`;
+
+    try {
+        const resp = await axios.get(url);
+          this.incrementPage();
+          return resp.data;
+        
+        }
+          catch (error) {
+              return error;
+          }
+  }
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+    incrementPage() {
+    this.page += 1;
+  }
+}
